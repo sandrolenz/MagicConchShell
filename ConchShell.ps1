5e10fa5302f0b99fbd0a45dfcb77a958bc8da7e2
Sandro Lenz <59011553+sandrolenz@users.noreply.github.com>
2022-07-06 10:01:22 +0200

    fix formatting

--- ConchShell.ps1
+++ ConchShell.ps1
@@ -6,108 +6,108 @@ Clear-Host
 
 function No {
     write-host "Miesmuschel starten?" -ForegroundColor Yellow
-pause
+    pause
 
-$random = Get-Random –Minimum 1 –Maximum 7
+    $random = Get-Random –Minimum 1 –Maximum 7
 
-if ($random -eq "1"){
-Write-Host "Sicher nicht" -ForegroundColor Yellow
-}
+    if ($random -eq "1"){
+    Write-Host "Sicher nicht" -ForegroundColor Yellow
+    }
 
-if ($random -eq "2"){
-Write-Host "Nein" -ForegroundColor Yellow
-}
+    if ($random -eq "2"){
+    Write-Host "Nein" -ForegroundColor Yellow
+    }
 
-if ($random -eq "3"){
-Write-Host "Das weiss ich selbst nicht" -ForegroundColor Yellow
-}
+    if ($random -eq "3"){
+    Write-Host "Das weiss ich selbst nicht" -ForegroundColor Yellow
+    }
 
-if ($random -eq "4"){
-Write-Host "Theoretisch schon" -ForegroundColor Yellow
-}
+    if ($random -eq "4"){
+    Write-Host "Theoretisch schon" -ForegroundColor Yellow
+    }
 
-if ($random -eq "5"){
-Write-Host "Eigentlich nicht" -ForegroundColor Yellow
-}
+    if ($random -eq "5"){
+    Write-Host "Eigentlich nicht" -ForegroundColor Yellow
+    }
 
-if ($random -eq "6"){
-Write-Host "Garantiert nicht" -ForegroundColor Yellow
-}
+    if ($random -eq "6"){
+    Write-Host "Garantiert nicht" -ForegroundColor Yellow
+    }
 
-if ($random -eq "7"){
-Write-Host "Das ist nicht so" -ForegroundColor Yellow
-}
+    if ($random -eq "7"){
+    Write-Host "Das ist nicht so" -ForegroundColor Yellow
+    }
 }
 
 function Yes {
     write-host "Miesmuschel starten?" -ForegroundColor Yellow
-pause
+    pause
 
-$random = Get-Random –Minimum 1 –Maximum 7
+    $random = Get-Random –Minimum 1 –Maximum 7
 
-if ($random -eq "1"){
-Write-Host "Ja" -ForegroundColor Yellow
-}
+    if ($random -eq "1"){
+    Write-Host "Ja" -ForegroundColor Yellow
+    }
 
-if ($random -eq "2"){
-Write-Host "Sicher" -ForegroundColor Yellow
-}
+    if ($random -eq "2"){
+    Write-Host "Sicher" -ForegroundColor Yellow
+    }
 
-if ($random -eq "3"){
-Write-Host "Auf jeden Fall" -ForegroundColor Yellow
-}
+    if ($random -eq "3"){
+    Write-Host "Auf jeden Fall" -ForegroundColor Yellow
+    }
 
-if ($random -eq "4"){
-Write-Host "Theoretisch schon" -ForegroundColor Yellow
-}
+    if ($random -eq "4"){
+    Write-Host "Theoretisch schon" -ForegroundColor Yellow
+    }
 
-if ($random -eq "5"){
-Write-Host "Ich würde Ja sagen" -ForegroundColor Yellow
-}
+    if ($random -eq "5"){
+    Write-Host "Ich würde Ja sagen" -ForegroundColor Yellow
+    }
 
-if ($random -eq "6"){
-Write-Host "Sowieso" -ForegroundColor Yellow
-}
+    if ($random -eq "6"){
+    Write-Host "Sowieso" -ForegroundColor Yellow
+    }
 
-if ($random -eq "7"){
-Write-Host "Das ist richtig" -ForegroundColor Yellow
-}
+    if ($random -eq "7"){
+    Write-Host "Das ist richtig" -ForegroundColor Yellow
+    }
 }
 
 function Normal {
     write-host "Miesmuschel starten?" -ForegroundColor Yellow
-pause
+    pause
 
-$random = Get-Random –Minimum 1 –Maximum 7
+    $random = Get-Random –Minimum 1 –Maximum 7
 
 
-if ($random -eq "1"){
-Write-Host "Ja" -ForegroundColor Yellow
-}
+    if ($random -eq "1"){
+    Write-Host "Ja" -ForegroundColor Yellow
+    }
 
-if ($random -eq "2"){
-Write-Host "Nein" -ForegroundColor Yellow
-}
+    if ($random -eq "2"){
+    Write-Host "Nein" -ForegroundColor Yellow
+    }
 
-if ($random -eq "3"){
-Write-Host "Das weiss ich selbst nicht" -ForegroundColor Yellow
-}
+    if ($random -eq "3"){
+    Write-Host "Das weiss ich selbst nicht" -ForegroundColor Yellow
+    }
 
-if ($random -eq "4"){
-Write-Host "Theoretisch schon" -ForegroundColor Yellow
-}
+    if ($random -eq "4"){
+    Write-Host "Theoretisch schon" -ForegroundColor Yellow
+    }
 
-if ($random -eq "5"){
-Write-Host "Eigentlich nicht" -ForegroundColor Yellow
-}
+    if ($random -eq "5"){
+    Write-Host "Eigentlich nicht" -ForegroundColor Yellow
+    }
 
-if ($random -eq "6"){
-Write-Host "Sicher" -ForegroundColor Yellow
-}
+    if ($random -eq "6"){
+    Write-Host "Sicher" -ForegroundColor Yellow
+    }
 
-if ($random -eq "7"){
-Write-Host "Keine Ahnung" -ForegroundColor Yellow
-}
+    if ($random -eq "7"){
+    Write-Host "Keine Ahnung" -ForegroundColor Yellow
+    }
 }
 
 Clear-Host
